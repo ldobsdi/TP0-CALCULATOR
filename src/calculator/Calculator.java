cfabebff1248108d6ac30e6888622497573189c6
doria <doria@Dorian-Desktop.home>
2021-10-02 19:48:55 +0200

    CREATION DU DEPOT

---
+++ src/calculator/Calculator.java
@@ -0,0 +1,21 @@
+/*
+* TP0 Exercice 1
+ * Dorian Guérin
+ * 29 Sept 2021
+ */
+package calculator;
+
+/**
+ *
+ * @author doria
+ */
+public class Calculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
